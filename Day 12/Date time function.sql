28e6b23459dc778c146cf57ab6e448a857c09418
shekhawatsoumya <shekhawatsoumya2@gmail.com>
2026-02-02 16:48:37 +0530

    Date Time Function

--- Day 12/Date time function.sql
+++ Day 12/Date time function.sql
@@ -44,4 +44,27 @@ select hour(now());
 select minute(now());
 select second(now());
 
+use dummy;
+#find the no. of days between the requireddate and shippeddate of each order?
+select datediff(requireddate,shippeddate),ordernumber from orders;
+
+#wasq to get all orders whose status are in process and calculate the no of days between order date and required date?
+select datediff(requireddate,orderdate),ordernumber from orders
+where status ="in process";
+
+#calculate the days between order date and shipped date and display N/A when the shipped date is null?
+select ordernumber,ifnull(datediff(shippeddate,orderdate),"n/a") from orders;
+
+#wasq to get ordercount for each quarter of the year 2004?
+select count(ordernumber),quarter(orderdate) from orders
+where year(orderdate) = 2004
+group by 2;
+
+#wasq to get ordercount of each quarter of each year?
+select count(ordernumber),year(orderdate),quarter(orderdate) from orders
+group by 2,3;
+
+#wasq to get ordercount of each quarter?
+select count(ordernumber),quarter(orderdate) from orders
+group by 2;
 
