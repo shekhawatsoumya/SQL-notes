65b6ac3556b17169355a8a6cde474ebfadc002c0
shekhawatsoumya <shekhawatsoumya2@gmail.com>
2026-02-05 16:03:44 +0530

    Math Function
    Added SQL queries to analyze order details and daily sales.

--- Day 14/Math Function.sql
+++ Day 14/Math Function.sql
@@ -29,6 +29,29 @@ select mod(2,5);
 #2/5 if numerator is smaller than denominator than is returns the dividend value.
 select mod(3,6);
 
+use dummy;
+
+#wasq to fetch ordernumber and the item sold per order on the basis of
+#item sold per order create on custom column odd or even?
+with cte as
+(select ordernumber,sum(quantityordered) as itemsoldperorder from orderdetails
+group by 1)
+select *,
+case
+when itemsoldperorder%2 then "odd"
+else "even"
+end as columnoddeven from cte;
+
+
+use dummy;
+#wasq to fetch dailysales of each productline of second and third quarter of each year(fetch year(orderdate),dayname and total sales))?
+select year(orderdate),sum(quantityordered*priceeach) as tov,dayname(orderdate),productline from orders 
+join orderdetails using(ordernumber) join products using(productcode)
+where quarter(orderdate) in (2,3)
+group by 1,3,4
+order by productline;
+
+
 
 
 
