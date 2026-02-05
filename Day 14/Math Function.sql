68fd4be43d42edef9e5383b1e883652bf91bee2e
shekhawatsoumya <shekhawatsoumya2@gmail.com>
2026-02-05 16:03:08 +0530

    Math Function

--- Day 14/Math Function.sql
+++ Day 14/Math Function.sql
@@ -29,46 +29,6 @@ select mod(2,5);
 #2/5 if numerator is smaller than denominator than is returns the dividend value.
 select mod(3,6);
 
-use dummy;
-
-#wasq to fetch ordernumber and the item sold per order on the basis of
-#item sold per order create on custom column odd or even?
-with cte as
-(select ordernumber,sum(quantityordered) as itemsoldperorder from orderdetails
-group by 1)
-select *,
-case
-when itemsoldperorder%2 then "odd"
-else "even"
-end as columnoddeven from cte;
-
-
-use dummy;
-#wasq to fetch dailysales of each productline of second and third quarter of each year(fetch year(orderdate),dayname and total sales))?
-select year(orderdate),sum(quantityordered*priceeach) as tov,dayname(orderdate),productline from orders 
-join orderdetails using(ordernumber) join products using(productcode)
-where quarter(orderdate) in (2,3)
-group by 1,3,4
-order by productline;
-
-use dummy;
-
-#truncate(x,d)
-# here x is value, d is upto decimals.
-
-select truncate(199.99,1); #199.9
-select truncate(199.99,0); #199
-select truncate(199.99,2); #199.99
-
-#Round(x,[d])
-# x is value/number, d is upto decimal
-# d is by default 0.
-
-select round(199.99,1); #200.0
-select round(199.99); #200
-select round(199.999,2); #200.00
-select round(199.35,1); #199.4
-select round(64.4453,2); #64.45
 
 
 
